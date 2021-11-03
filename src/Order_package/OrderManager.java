1d19d0902ca52aa0633ceb0a2633433004b56ea8
hwyiing <pics.cbj@gmail.com>
2021-11-03 17:25:25 +0800

    sync changes

--- src/Order_package/OrderManager.java
+++ src/Order_package/OrderManager.java
@@ -1,5 +1,9 @@
 package Order_package;
 
 public class OrderManager {
-    
+    private Order[][] dayOrders;
+
+    public Order[][] getDayOrders(){
+        return this.dayOrders; 
+    }
 }
---
+++ src/SalesReport_package/SalesReport.java
@@ -0,0 +1,20 @@
+package SalesReport_package;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Order_package.Order;
+import Reservation_package.Tables;
+
+public class SalesReport {
+    
+    public writeDayReport(){
+        //loop through the Restaurant's tables and the orders stored inside 
+        
+    }
+    
+
+
+}
+
