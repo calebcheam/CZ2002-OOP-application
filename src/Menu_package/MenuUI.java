2396508edf6a43b4d1d95d60499b5f1b107d608a
briank0h <92429258+briank0h@users.noreply.github.com>
2021-11-07 00:17:58 +0800

    Update MenuUI.java

--- src/Menu_package/MenuUI.java
+++ src/Menu_package/MenuUI.java
@@ -9,10 +9,10 @@ public class MenuUI {
 	
 	public void start() {
 		do {
-			System.out.println("============================");
+		System.out.println("============================");
 	        System.out.println("|       Menu Options       |");
 	        System.out.println("============================");
-	        System.out.println("Options:");
+	        System.out.println("Options:                    ");
 	        System.out.println("1. Display Menu             ");
 	        System.out.println("2. Add new item             ");
 	        System.out.println("3. Remove existing item     ");
@@ -23,8 +23,7 @@ public class MenuUI {
 	        System.out.print("Enter Option: ");
 	        choice = sc.nextInt();
 	        switch(choice) {
-	        case 1:
-	        	
+	        case 1:	        	
 	        break;
 	        case 2:
 	        break;
@@ -35,15 +34,13 @@ public class MenuUI {
 	        case 5:
 	        break;
 	        case 6:
-	        	System.out.print("Menu Interface closed");
+	        	System.out.println("Menu Interface closed");
 	        break;
 	        }
 		}while (choice != 6);
 	}
-
 	public static void main(String[] args) {
 		MenuUI menuui= new MenuUI();
 		menuui.start();
 	}
-
 }
