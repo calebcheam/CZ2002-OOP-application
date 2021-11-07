e27199469bb1ead385453498490d8da2d7fd4be9
Take-Saori <71936501+Take-Saori@users.noreply.github.com>
2021-11-07 11:06:33 +0800

    Delete MenuItemTypeInvalid.java

--- src/Menu_package/MenuItemTypeInvalid.java
+++
@@ -1,6 +0,0 @@
-package Menu_package;
-public class MenuItemTypeInvalid extends Exception{
-    public MenuItemTypeInvalid(String itemType){
-        super("Menu Item's Type \"" +itemType+ "\" is invalid!");
-    }
-}
