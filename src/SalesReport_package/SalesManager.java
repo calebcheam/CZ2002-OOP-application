fcee8cef4213413dd233c1ffc94a385eed11b799
rkambai <rkam002@e.ntu.edu.sg>
2021-11-13 02:20:12 +0800

    javadoc of sales manager

--- src/SalesReport_package/SalesManager.java
+++ src/SalesReport_package/SalesManager.java
@@ -2,16 +2,30 @@ package SalesReport_package;
 
 import java.util.Scanner;
 
+/**
+ * <code>SalesManager</code> class that handles all sales related activities, including storing and fetching sales data
+ * @see DailySaleReport
+ * @see SaleReports
+ * @author DSAI1 ASSIGNMENT GROUP 3
+ * @version 1.0
+ */
+
 public class SalesManager {
     private static DailySaleReport dsr=new DailySaleReport();
     private static SaleReports SR=new SaleReports(); 
     private static Scanner sc=new Scanner(System.in);
 
+    /** 
+     * Displays the current day's sales report
+     */
     public static void viewTodaySales(){
         dsr = new DailySaleReport();
         dsr.printFromObject();
     }
 
+    /** 
+     * Saves the current day's sales report to a csv
+     */
     public static void saveTodaySales(){
         dsr = new DailySaleReport();
         dsr.printFromObject();
@@ -27,11 +41,21 @@ public class SalesManager {
 
     }
 
+    /** 
+     * Displays all sales reports recorded to date
+     */
     public static void viewAllSaleReport(){
         SR = new SaleReports();
         SR.printAllReports();
     }
 
+    
+    
+    /** 
+     * Displays sales from a selected date if <code>boolean</code> value of <code>true</code< is passed in as parameters.
+     * Else, prompts user to select a time period and displays sales over that period
+     * @param viewAll <code>boolean</code> true views all sales, false enables time period selection
+     */
     public static void viewAccumulatedSales(boolean viewAll){
         
         SR = new SaleReports();
@@ -62,6 +86,9 @@ public class SalesManager {
         
     }
 
+    /** 
+     * Displays the sales report of a single selected day
+     */
     public static void viewOneSaleReport(){
         SR = new SaleReports();
         String[] datesArray = SR.getReportDates();
@@ -72,9 +99,12 @@ public class SalesManager {
 
     }
     
+
+    
     /** 
-     * Displays the dates of each sales report
-     * @param startIndex
+     * Displays the dates of each sales report consecutively
+     * @param dates <code>String[]</code> containing dates to be passed in
+     * @param startIndex <code>int</code> index of starting date to be printed from
      */
     private static void printDates(String[] dates, int startIndex){
         if (startIndex==dates.length-1) return;
